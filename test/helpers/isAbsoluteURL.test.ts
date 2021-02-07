e443b9e3c1e662c44fd09c6fffd13120d5c4e654
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-02-07 20:59:51 +0800

    test: Add isAbsoluteURL test case

---
+++ test/helpers/isAbsoluteURL.test.ts
@@ -0,0 +1,33 @@
+import isAbsoluteURL from "../../src/helpers/isAbsoluteURL";
+
+describe("isAbsoluteURL", () => {
+  it("not absolute url", () => {
+    expect(isAbsoluteURL("")).toBeFalsy();
+
+    expect(isAbsoluteURL("xxx.com")).toBeFalsy();
+
+    expect(isAbsoluteURL()).toBeFalsy();
+
+    expect(isAbsoluteURL(null)).toBeFalsy();
+
+    expect(isAbsoluteURL(undefined)).toBeFalsy();
+
+    expect(isAbsoluteURL(123)).toBeFalsy();
+
+    expect(isAbsoluteURL(true)).toBeFalsy();
+
+    expect(isAbsoluteURL("19.14.23.12")).toBeFalsy();
+  });
+
+  it("is absolute url", () => {
+    expect(isAbsoluteURL("http://19.14.23.12")).toBeTruthy();
+
+    expect(isAbsoluteURL("https://19.14.23.12")).toBeTruthy();
+
+    expect(isAbsoluteURL("http://xxx.com")).toBeTruthy();
+
+    expect(isAbsoluteURL("https://www.xxx.com")).toBeTruthy();
+
+    expect(isAbsoluteURL("//www.xxx.com")).toBeTruthy();
+  });
+});
