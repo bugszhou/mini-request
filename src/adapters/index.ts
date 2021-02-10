aa4ee53a309bea3ac1c025251613467accaeaff6
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-02-10 13:21:17 +0800

    feat: 修改default adapter

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,11 +1,33 @@
-import { isFunction, isUndefined } from "../helpers/utils";
-import wxRequest from "./wx/weappRequest";
+import transformMethod from "../helpers/transformMethod";
+import Adapter from "./Adapter";
 
-export type IAdapterFn = (config: IAppletsRequestConfig) => IAppletsRequestPromise;
+export type IAdapterFn = (
+  config: IAppletsRequestConfig
+) => IAppletsRequestPromise;
 
 export default function getDefaultAdapter(): IAdapterFn {
-  if (!isUndefined(wx) && isFunction(wx.request)) {
-    return wxRequest as IAdapterFn;
-  }
-  return Promise.resolve;
+  return defaultAdapter;
+}
+
+function defaultAdapter(config: IAppletsRequestConfig): IAppletsRequestPromise {
+  console.error(
+    "defaultAdapter: ",
+    "Pls set adapter!!! Don't use default adapter"
+  );
+  console.error("defaultAdapter: ", config);
+  const adapter = new Adapter({
+    method: transformMethod(config.method),
+    ...config,
+  } as IAppletsRequest.IHttpConfig);
+  return new Promise((resolve) => {
+    adapter.resolve(
+      {
+        data: {},
+        headers: {},
+        status: 404,
+        response: null,
+      },
+      resolve
+    );
+  });
 }
