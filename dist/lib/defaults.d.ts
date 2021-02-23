f1205e956686d776089fec2775fe2f7efef210a8
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-02-23 18:59:34 +0800

    chore: Release 1.0.8

--- dist/lib/defaults.d.ts
+++ dist/lib/defaults.d.ts
@@ -1,2 +1,2 @@
 export declare const STORAGE_COOKIES_KEY = "miniRequest:cookies";
-export default function getDefaults(): IAppletsRequestConfig;
+export default function getDefaults(): IAppletsRequestConfig<IAppletsRequest.IDataType, any>;
